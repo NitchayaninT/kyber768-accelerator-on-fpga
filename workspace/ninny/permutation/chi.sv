50f795c16af52812e79f567ad0a5195e82892791
Pakin49 <pakin.pan@proton.me>
2025-11-03 01:24:08 +0700

    At least there are some output from our shake even though it's completely wrong

--- workspace/ninny/permutation/chi.sv
+++ workspace/ninny/permutation/chi.sv
@@ -1,8 +1,17 @@
 // XOR each bit with a non-linear function of two other bits in its row
 module chi (
-    input  [63:0] state_in[0:24],
-    output [63:0] state_out[0:24]
+    input  [1599:0] state_in,
+    output [1599:0] state_out
 );
+  // Unpack state into lanes with 64 bits
+  wire [63:0] A_in [0:24];
+  wire [63:0] A_out[0:24];
+  genvar i;
+  generate
+    for (i = 0; i < 25; i = i + 1) begin : unpacking
+      assign A_in[i] = state_in[i*64+:64];  //assign 64 bits to each lane
+    end
+  endgenerate
 
   // function to AND and XOR based on column number (lane % 5)
   // return CURRENT lane after XOR and AND
@@ -25,8 +34,16 @@ module chi (
         localparam integer lane_no = x + (5 * y);
         localparam integer lane1_no = (y * 5) + (lane_no + 1) % 5;
         localparam integer lane2_no = (y * 5) + (lane_no + 2) % 5;
-        assign A_out[lane_no] = and_xor(state_in[lane_no], state_in[lane1_no], state_in[lane2_no]);
+        assign A_out[lane_no] = and_xor(A_in[lane_no], A_in[lane1_no], A_in[lane2_no]);
       end
+    end
+  endgenerate
+
+  // pack to state_out, len by len
+  genvar j;
+  generate
+    for (j = 0; j < 25; j = j + 1) begin : packing
+      assign state_out[j*64+:64] = A_out[j];
     end
   endgenerate
 endmodule
