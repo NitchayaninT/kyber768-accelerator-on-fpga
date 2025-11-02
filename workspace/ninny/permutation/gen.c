6f850d52af8432a4dbe8cbb8662a8b0bd16672e3
Pakin49 <pakin.pan@proton.me>
2025-11-02 23:26:19 +0700

    update topmodule

---
+++ workspace/ninny/permutation/gen.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  for (int i = 0; i < 48; i++) {
+    printf("A");
+  }
+
+  return 0;
+}
--- workspace/ninny/permutation/rho_tb.sv
+++ workspace/ninny/permutation/rho_tb.sv
@@ -1,3 +1,23 @@
+`timescale 1ns/1ps
+`define DELAY 10
 module rho_tb;
+  reg [1599:0] in;
+  wire  [63:0] state_in[0:24];
+  wire [63:0] state_out[0:24];
 
+  genvar i;
+  generate
+    for (i = 0; i < 25; i = i + 1) begin : unpacking
+      assign state_in[i] = in[i*64+:64];  //assign 64 bits to each lane
+    end
+  endgenerate
 
+  rho rho_uut(.state_in(state_in), .state_out(state_out));
+
+  initial begin
+    in = {1408'h0, 192'hAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA};
+    $monitor("state_out[0]:%h\nstate_out[1]=%h\nstate_out[3]=%h",state_out[0],state_out[1],state_out[2]);
+    #(`DELAY*50);
+    $finish;
+  end
+endmodule
