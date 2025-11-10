0ec9f22703eb5011a034b0f125222e4ea8679ea9
Pakin49 <pakin.pan@proton.me>
2025-11-10 12:21:35 +0700

    shake_tb monitor endian every round

--- workspace/ninny/permutation/shake_tb.sv
+++ workspace/ninny/permutation/shake_tb.sv
@@ -35,25 +35,31 @@ module shake_tb;
       */
   );
 
-  task print_lane_bytes(input [63:0] lane, input integer idx);
-    integer k;
-    begin
-      $write("%2d: ", idx);
-      for (k = 0; k < 8; k = k + 1) $write("%02h", lane[8*k+:8]);  // b0..b7 
-      $write("\n");
-    end
-  endtask
+
+  always @(shake_uut.round) begin
+    print_state_bytes(shake_uut.state_out);
+    $display("INPUT (before theta)");
+    print_state_bytes(shake_uut.theta_out);
+    $display("After theta (round %d)", shake_uut.round);
+    print_state_bytes(shake_uut.rho_out);
+    $display("After rho   (round %d)", shake_uut.round);
+    print_state_bytes(shake_uut.pi_out);
+    $display("After pi    (round %d)", shake_uut.round);
+    print_state_bytes(shake_uut.chi_out);
+    $display("After chi   (round %d)", shake_uut.round);
+    print_state_bytes(shake_uut.iota_out);
+    $display("After iota  (round %d)", shake_uut.round);
+  end
 
   task print_lane_bytes_be(input [63:0] lane, input integer idx);
     $display("%2d: %02h%02h%02h%02h%02h%02h%02h%02h", idx, lane[63:56], lane[55:48], lane[47:40],
              lane[39:32], lane[31:24], lane[23:16], lane[15:8], lane[7:0]);
   endtask
 
-  task print_state_bytes(input [1599:0] S, input [1023:0] tag);
+  task print_state_bytes(input [1599:0] S);
     integer i;
     reg [63:0] lane;
     begin
-      $display("\n%s", tag);
       for (i = 0; i < 25; i = i + 1) begin
         lane = S[i*64+:64];  // i = x + 5*y 
         print_lane_bytes_be(lane, i);
@@ -64,8 +70,8 @@ module shake_tb;
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars(0, shake_tb);
-    $monitor("time:%t\n state_out: %h\n round: %d\n valid:%b", $time, state_out, shake_uut.round,
-             valid);
+    //$monitor("time:%t\n state_out: %h\n round: %d\n valid:%b", $time, state_out, shake_uut.round,
+    //        valid);
     clk = 0;
     forever #(`DELAY / 2) clk = ~clk;
   end
@@ -77,6 +83,7 @@ module shake_tb;
     #(`DELAY) rst = 0;
     #(`DELAY) enable = 1;
 
+    /*
     #1;
     print_state_bytes(shake_uut.state_out, "INPUT (before theta)");
     print_state_bytes(shake_uut.theta_out, "After theta (round 0)");
@@ -84,10 +91,11 @@ module shake_tb;
     print_state_bytes(shake_uut.pi_out, "After pi    (round 0)");
     print_state_bytes(shake_uut.chi_out, "After chi   (round 0)");
     print_state_bytes(shake_uut.iota_out, "After iota  (round 0)");
-
+    */
     #(`DELAY) enable = 0;
     #(`DELAY) rst = 0;
     #(`DELAY) enable = 1;
+    /*
     #1;
     print_state_bytes(shake_uut.state_out, "INPUT (before theta)");
     print_state_bytes(shake_uut.theta_out, "After theta (round 1)");
@@ -95,7 +103,7 @@ module shake_tb;
     print_state_bytes(shake_uut.pi_out, "After pi    (round 1)");
     print_state_bytes(shake_uut.chi_out, "After chi   (round 1)");
     print_state_bytes(shake_uut.iota_out, "After iota  (round 1)");
-
+    */
     #(`DELAY * 50);
     $display("\n\nvalid : %b\n state_out = %h\n", valid, state_out);
     $display("answer : bc560b74bafdfcec6bef89337da01de833c65309e7e3cb6cfff9f5a263aabe16");
