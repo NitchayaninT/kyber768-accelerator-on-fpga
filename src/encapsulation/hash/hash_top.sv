3b3b51db3c83091bb3fc159d7116d6ffd03cda63
NitchayaninT <ninnycindy03@gmail.com>
2026-01-16 15:34:46 +0900

    done and verified cbd, rejection samp. and added shake256 for coin generation

--- src/encapsulation/hash/hash_top.sv
+++ src/encapsulation/hash/hash_top.sv
@@ -1,5 +1,6 @@
 // HASH top module
 // Workflow
+// Seed 256 bits -> SHAKE128 -> 5376 bits public matrix A transpose -> Reject sampling -> 9 polynomials of degree 256 with coeffs in [0,3328]
 // Coins 256 bits -> SHAKE128 -> 1024 bits noise -> CBD -> polynomial of degree 256 with coeffs in [-2,2]
 `timescale 1ns / 1ps
 module hash_top(
@@ -7,30 +8,55 @@ module hash_top(
     input rst,
     input enable,
     input [255:0] coins,
-    output reg done,
-    output reg [1023:0] poly_out
+    input [255:0] seed,
+    output reg noise_done,
+    output reg public_matrix_done,
+    output reg [4095:0] public_matrix_poly_out,
+    output reg [4095:0] noise_poly_out
 );
-    wire shake_done;
-    wire [5375:0] shake_stream;
+    wire shake_noise_done;
+    wire shake_public_matrix_done;
+    wire [5375:0] noise_stream;
+    wire [5375:0] public_matrix_stream;
 
-    shake128 shake128_module (
+    shake256 shake256_coin (
         .clk(clk),
         .enable(enable),
         .rst(rst),
         .in(coins),
         .domain(4'b1111), // domain separator for noise generation
         .output_len(14'd1024), // output length 1024 bits
-        .output_string(shake_stream),
-        .done(shake_done) // not used here
+        .output_string(noise_stream),
+        .done(shake_noise_done)
+    );
+
+    shake128 shake128_public_matrix (
+        .clk(clk),
+        .enable(enable),
+        .rst(rst),
+        .in(seed),
+        .domain(4'b1111), // domain separator for public matrix generation
+        .output_len(14'd5376), // output length 5376 bits
+        .output_string(public_matrix_stream),
+        .done(shake_public_matrix_done)
     );
 
     cbd cbd_module (
         .clk(clk),
         .rst(rst),
-        .enable(shake_done),
-        .noise(shake_stream[1023:0]),
-        .done(done),
-        .poly_out(poly_out)
+        .enable(shake_noise_done),
+        .noise(noise_stream),
+        .done(noise_done),
+        .poly_out(noise_poly_out)
+    );
+
+    reject_sampling reject_sampling_module (
+        .clk(clk),
+        .rst(rst),
+        .enable(shake_public_matrix_done),
+        .byte_stream(public_matrix_stream),
+        .done(public_matrix_done),
+        .public_matrix_poly(public_matrix_poly_out)
     );
 
 endmodule
--- src/encapsulation/hash/sampling_rejection/cbd.sv
+++ src/encapsulation/hash/sampling_rejection/cbd.sv
@@ -12,33 +12,43 @@ module cbd
     input enable,
     input [1023:0] noise,
     output reg done,
-    output reg [1023:0] poly_out // 256 coeffs, each coeff is 4 bits. 128 bytes per poly
+    output reg [4095:0] poly_out // 256 coeffs, each coeff is 16 bits. 128 bytes per poly
 );
     // parameters
     localparam integer N = 256; // degree of polynomial
     localparam integer eta = 2; // for kyber768
-    localparam integer noise_len = 1024;
-    localparam integer coeff_width = 4; // each coeff is 4 bits
+    localparam integer noise_len = 4096;
+    localparam integer coeff_width = 16; // each coeff is 16 bits
+
+    // Reorder bytes
+    wire [1023:0] noise_reordered;
+    genvar j;
+    generate
+        for (j = 0; j < 128; j = j + 1) begin : REORDER
+            assign noise_reordered[j*8 +: 8] = noise[1023-8*j -:8];
+        end
+    endgenerate
 
     // for loop : from 0 to 255
     integer i;
     integer a, b;
-    logic signed [coeff_width-1:0] coeff;
+    logic signed [coeff_width-1:0] coeff; //16
     always @(posedge clk or posedge rst) begin
         // get a and b for each coeff
         // a = number of 1s in first eta bits (0,1,2)
         // b = number of 1s in next eta bits (0,1,2)
         // coef = a-b, range from [-2,2]
+        // but each coef has to be stored in a 16-bit format
         if(rst) begin
             done <= 1'b0;
-            poly_out <= 1024'b0;
+            poly_out <= 4096'b0;
         end else begin
             for(i = 0; i < N; i = i + 1) begin
                 a = 0;
                 b = 0;
                 for(integer j = 0; j < eta; j = j + 1) begin
-                    a = a + noise[i*(2*eta) + j];
-                    b = b + noise[i*(2*eta) + eta + j];
+                    a = a + noise_reordered[i*(2*eta)+ j];
+                    b = b + noise_reordered[i*(2*eta)+ eta + j];
                 end
                 coeff = $signed(a - b); // in [-2,2] range
                 poly_out[i*coeff_width +: coeff_width] <= coeff; // each coef consumes 4 bits. so 256*4 = 1024 bits are produced
@@ -46,5 +56,4 @@ module cbd
             done <= 1'b1;
         end
     end
-
 endmodule
