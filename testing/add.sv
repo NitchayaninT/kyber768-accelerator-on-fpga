c0110ee0c9878ff2427af419fd7c5db72a4fb78b
Pakin49 <pakin.pan@proton.me>
2026-01-21 14:36:22 +0900

    add link to params.vh

--- testing/add.sv
+++ testing/add.sv
@@ -1,5 +1,8 @@
-`timescale 1ns / 1ps
 `include "params.vh"
 
-// each coefficient in polynomial ring use on cla_adder
-// 4rounds of addition is made and then copy value from buffer to output
+module add(
+  input clk,
+  input [`KYBER_N ]
+
+);
+endmodule
