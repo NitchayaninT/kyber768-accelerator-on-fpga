a3eda632fbbb28c1980c6c3272aa91acca2e1bf0
Tony <panupong.sap@student.mahidol.edu>
2026-05-14 23:02:32 +0700

    finish decryption_top

--- hdl/encryption/pre_encryption/decode_pk.sv
+++ hdl/encryption/pre_encryption/decode_pk.sv
@@ -12,6 +12,7 @@ module decode_pk (
 );
   // Noted that concept of transpose in FPGA does not make much sense since we
   // just save in the net variables just wire differently
+  /*
   assign rho = public_key[255:0];
   // generate block for t_trans
   wire [(KYBER_RQ_WIDTH * KYBER_N)-1:0] t_raw[3];
@@ -41,5 +42,41 @@ module decode_pk (
   assign t_trans[0] = t_reordered[2];
   assign t_trans[1] = t_reordered[1];
   assign t_trans[2] = t_reordered[0];
+  assign done = 1'b1;*/
+  //public_key = 256 + 3 * 3072, which is 32 bytes and 3 384 bytes
+  //t0 - t2 each 384 bytes (1152 bytes), rho is 32 bytes
+  // total = 1184 bytes
+  genvar p, i, b;
+  // copy byte 1152 - 1183 to rho
+  generate
+    for (b = 0; b < 32; b++) begin : G_RHO
+      assign rho[255 - 8*b -: 8] = public_key[8*(1152 + b) +: 8];
+    end
+  endgenerate
+  // coeff0 = (byte0 | (byte1 << 8)) & 0xfff;
+  // coeff1 = (byte1 >> 4) | (byte2 << 4)  & 0xfff;
+  /*byte0:  [ coeff0 bits 0..7 ]
+
+    byte1:  [ coeff1 bits 0..3 ][ coeff0 bits 8..11 ]
+
+    byte2:  [ coeff1 bits 4..11 ]
+    every 2 coefficients are packed into 3 bytes
+  */
+  generate
+    for (p = 0; p < KYBER_K; p++) begin : G_POLY
+      for (i = 0; i < KYBER_N/2; i++) begin : G_COEFF
+        localparam int BASE_BYTE = p*384 + 3*i;
+
+        assign t_trans[p][12*(2*i) +: 12] =
+            ({4'b0, public_key[8*(BASE_BYTE+0) +: 8]} |
+             ({4'b0, public_key[8*(BASE_BYTE+1) +: 8]} << 8)) & 12'hFFF;
+
+        assign t_trans[p][12*(2*i+1) +: 12] =
+            (({4'b0, public_key[8*(BASE_BYTE+1) +: 8]} >> 4) |
+             ({4'b0, public_key[8*(BASE_BYTE+2) +: 8]} << 4)) & 12'hFFF;
+      end
+    end
+  endgenerate
+
   assign done = 1'b1;
 endmodule
